52ed0f80a6ef8b67d208bd082afe9fea4ec95cd4
Praphul Kumar <praphul.test@gmail.com>
2023-08-25 17:45:08 +0530

    Parsing Url and sending custom header in response

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
-    res.end("Hello from the server!!");
+
+    const uriPath = req.url;
+
+    if (uriPath === '/' || uriPath === '/overview') {
+        res.end('This is OVERVIEW');
+    } else if (uriPath === "/product") {
+        res.end("This is PRODUCT");
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end("<h1>Page Not Found!!</h1>");
+    }
 });
 
 server.listen(8000, '127.0.0.1', () => {
