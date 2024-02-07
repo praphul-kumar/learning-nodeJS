265772d3ddcc593d7d4776287f70ad52cf05d412
Praphul Kumar <praphulkumar2023@gmail.com>
2024-02-07 13:18:42 +0530

    Cache module in nodeJS

--- basics/modules/modules.js
+++ basics/modules/modules.js
@@ -12,6 +12,21 @@
 // console.log(calc.add(10, 20));
 
 // * Importing module of: exports
-const calc = require('./test-module-2');
 
-console.log(calc.add(10, 20));
+// Example 01:
+// const calc = require('./test-module-2');
+
+// console.log(calc.add(10, 20));
+
+// Example 02:
+// const {add, multiply} = require('./test-module-2');
+// console.log(add(10, 30));
+
+// * Caching module
+require('./test-module-3')();
+require('./test-module-3')();
+require('./test-module-3')();
+
+// Here you can see in the output that:
+// we have only one console log that is the module but we are requiring it three time 
+// but due to cache it is required only one time where as the function runs three time.
---
+++ basics/modules/test-module-3.js
@@ -0,0 +1,5 @@
+console.log('Hello from this module');
+
+module.exports = () => {
+  console.log('Funtion is Executed 😊');
+}
