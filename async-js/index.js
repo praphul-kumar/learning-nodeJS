eeaf00ea89e12c6e5e6ed37cafc33da74451f300
Praphul Kumar <praphulkumar2023@gmail.com>
2024-05-31 10:38:41 +0530

    Understanding Callback hell

---
+++ async-js/index.js
@@ -0,0 +1,24 @@
+// Understanding Problems with callbacks Hell
+
+const fs = require("fs");
+const superagent = require('superagent');
+
+fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+  console.log(`Breed: ${data}`);
+
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .end((err, res) => {
+      if (err) return console.log(err.message);
+
+      console.log(res.body.message);
+
+      fs.writeFile('dog-img.txt', res.body.message, err => {
+        console.log('Random dog image saved to file!!');
+      })
+    })
+});
+
+// You can see in the above example there are multilevel callbacks 
+// it can be really messy when there are so many lavels of callbacks used in a project beacuse it is harder to maintain.
+
